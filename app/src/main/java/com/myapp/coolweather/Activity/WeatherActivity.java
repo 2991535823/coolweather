2223c5e3f53c7cae30d3e1a017f91a5bc66905d3
zgm <2991535823@qq.com>
2020-04-04 17:06:44 +0800

    ui done

---
+++ app/src/main/java/com/myapp/coolweather/Activity/WeatherActivity.java
@@ -0,0 +1,16 @@
+package com.myapp.coolweather.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.myapp.coolweather.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
